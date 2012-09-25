6cd0ac31585d9a846c81d2728e478d35737e74f8
imedina <imedina@cipf.es>
2012-09-25 19:42:43 +0200

    GPLv2 license added to header and sources files

--- src/chaos_game.c
+++ src/chaos_game.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2012 Victor Requena (BULL)
+ * Copyright (c) 2012 Ignacio Medina (CGI-CIPF)
+ *
+ * This file is part of hpg-fastq.
+ *
+ * hpg-fastq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hpg-fastq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hpg-fastq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include <omp.h>
 
